06ef628d8e019ec06e63dbdf929bf2c6442908cc
cytvictor <cytvictor@yeah.net>
2018-02-18 09:59:11 +0800

    完成提交申请/TODO:限制端口数量

--- Application/v1/Controller/PortController.class.php
+++ Application/v1/Controller/PortController.class.php
@@ -6,6 +6,7 @@
  * Time: 12:32 PM
  */
 namespace v1\Controller;
+use v1\Model\IpsModel;
 use v1\Model\PortsModel;
 use v1\Model\VmsModel;
 
@@ -34,6 +35,40 @@ class PortController extends BaseController{
 
     public function apply()
     {
+        $vmid = I('get.vm');
+        $Vms = new VmsModel();
+        $Ips = new IpsModel();
+        $Ports = new PortsModel();
+        $vm_info = $Vms->where(array("id"=>$vmid,"uid"=>getUID()))->select()[0];
+        if (!$vm_info)//若信息不合法
+        {
+            $this->error('请在"全部虚拟机"部分选择要申请端口的虚拟机，然后选择右侧操作的"申请新端口"','/vms',4);
+            exit;
+        }
+
+        $ips_info = $Ips->where(array("host_id"=>$vm_info['host_id']))->select();
+        for ($i=0;$i<count($ips_info);$i++)
+        {
+            $first_ports_info = $Ports->where(array("ip_id"=>$ips_info[$i]['id'],"apply_status"=>"UNUSED"))->order('port asc')->select();
+
+            /*$J = 0;
+            $bigJ = 0;
+            foreach ($first_ports_info as $key => $value)
+            {
+                $second_ports_info[$bigJ][$J] = $first_ports_info[$J];
+                $J += 1;
+                if ($bigJ+1 % 8 == 0 ) $bigJ ++;
+            }*/
+
+
+            $ports_info[$ips_info[$i]['ip_address']] = $first_ports_info;
+        }
+
+
+
+        $this->assign('ips_info',$ips_info);
+        $this->assign('ports_info',$ports_info);
+        $this->assign('vm_info',$vm_info);
         $this->assign('SideBar_Selected','Port_Apply');
         $this->meta_title = '提交申请';
         $this->display();
@@ -65,6 +100,38 @@ class PortController extends BaseController{
             echo json_encode(array(
                 "success" => true,
                 "msg" => "成功释放端口"
+            ));
+        }
+    }
+
+    public function Action_submitPort()
+    {
+        //TODO:验证VM身份(Solved)
+        //TODO:限制申请数量
+        ApiResponseHeader();
+        $port_id = I('post.port_id');
+        $connect_port = I('post.connect_port');
+        $vm_id = I('post.vm_id');
+        $Ports = new PortsModel();
+        $Vms = new VmsModel();
+        $port_info = $Ports->where(array("id"=>$port_id,"apply_status"=>"UNUSED","status"=>"NORMAL"))->select()[0];
+        $vm_info = $Vms->where(array("id"=>$vm_id,"uid"=>getUID()))->select()[0];
+        if (empty($port_info) or empty($vm_info) or !is_integer(intval($connect_port)))
+        {
+            echo json_encode(array(
+                "error" => true,
+                "msg" => "发生错误，请刷新后重试。"
+            ));exit;
+        } else {
+            $Ports->where(array("id"=>$port_id))->data(array(
+                "connect_port" => $connect_port,
+                "port" => $port_info['port'],
+                "vm_id" => $vm_id,
+                "apply_status" => "APPLIED",
+            ))->save();
+            echo json_encode(array(
+                "success" => true,
+                "msg" => "已成功提出申请，请耐心等待管理员审核！"
             ));
         }
     }
